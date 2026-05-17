dadf6b63e3fe1315bf0eb469a162a9a403341b50
rorz <1853310+rorz@users.noreply.github.com>
2026-05-17 22:48:30 +0200

    add social links to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
+
 export const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="mt-12">
-      <p className="text-mist-500">&copy; Rory McMeekin {year}</p>
+    <footer className="mt-12 flex justify-between align-baseline text-mist-500">
+      <p>&copy; Rory McMeekin {year}</p>
+      <div className="flex gap-2 text-sm">
+        <Link target="_blank" href="https://github.com/rorz">
+          GitHub
+        </Link>
+        <Link target="_blank" href="https://linkedin.com/in/rorz">
+          LinkedIn
+        </Link>
+        <Link target="_blank" href="https://x.com/rorzio">
+          X
+        </Link>
+      </div>
     </footer>
   );
 };
